ee3b51014b9ae484019634b97e4cc5c938eb9141
Sattwik13 <sattwikmanna6359@gmail.com>
2024-05-22 13:29:55 +0530

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,47 @@
+const name = "SATTWIK"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is: ${name} and my repo count is: ${repoCount}`);
+
+const gameName = new String('SATTWIK')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);// -> object type
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('T')); // if T use uppercase, use uppercase neither lowercase
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString);
+
+const newStringOne = "   SATTWIK   "
+console.log(newStringOne);
+console.log(newStringOne.trim());// trim blank space
+
+
+const url = "https://Sattwik.com/sattwik%20Manna"
+
+console.log(url.replace('%20', '-')); // replace use
+console.log(url.includes('sattwik')); // if sattwik present or not, return true
+
+
+let Mname = "Sattwik-manna-25"
+
+console.log(Mname.split('-'));// convert an array, split by - separator
+
+const str1 = '5';
+
+console.log(str1.padStart(2, '0'));// Expected output: "05"
+
+const fullNumber = '2034399002125581';
+const last4Digits = fullNumber.slice(-4);
+const maskedNumber = last4Digits.padStart(fullNumber.length, '*');
+
+console.log(maskedNumber);// --> ************5581
