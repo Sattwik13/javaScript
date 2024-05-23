a1cd494f957124a8a79ce626f813e7d2033275c8
Sattwik13 <sattwikmanna6359@gmail.com>
2024-05-23 17:17:17 +0530

    Immediate Invoked Function Expression (IIFE)

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,22 @@
+// Immediate Invoked Function Expression (IIFE)
+// IIFE resolved global scope pollution
+
+// function chai() {
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+
+// (function chai() { // ->named IIFE
+//     console.log(`DB CONNECTED`);
+// })(); // --> first "()"-> defination & second "()" -> execution & ";" is mandatory beacause it's end invoked fuction
+
+
+// ( () => {
+//     console.log(`DB CONNECTED TO`);
+// }) ();
+
+
+((name) => {
+    console.log(`DB CONNECTED TO ${name}`);
+}) ('SATTWIK');
