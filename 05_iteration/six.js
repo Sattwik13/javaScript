e54d3391d2bbeca5d31ef382fb3ec4bf107d5309
Sattwik13 <sattwikmanna6359@gmail.com>
2024-05-24 18:07:25 +0530

    first commit

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -1,8 +1,11 @@
 const coding = ["js" , "rb", "py", "java", "cpp" ]
 
 const values = coding.forEach((item) => {
-
+   console.log(item);
+   return item
 })
+
+// console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
